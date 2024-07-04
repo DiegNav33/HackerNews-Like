fc1185da8eaed159a0300dcc92fda105670f4fe6
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-04 18:39:09 +0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+gem "binding_of_caller"
+gem "faker"
+gem "pry-byebug"
+gem 'sinatra', '~> 2.1'
+gem 'sinatra-contrib', '~> 2.1'
+gem "thin"
---
+++ lib/app.rb
@@ -0,0 +1,47 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "pry-byebug"
+
+require_relative "../config/application"
+
+set :root, File.expand_path("..", __dir__)
+set :views, proc { File.join(root, "app/views") }
+set :bind, '0.0.0.0'
+
+after do
+  ActiveRecord::Base.connection.close
+end
+
+# Route pour la page d'accueil affichant tous les posts
+get "/" do
+  @posts_desc = Post.all.order(votes: :desc)
+  @post = Post.new
+  erb :posts # Do not remove this line
+end
+
+post "/posts" do
+  title = params[:title]
+  url = params[:url]
+  user = User.first
+  @post= Post.create(title: title, url: url, user: user)
+
+  if @post.persisted?
+    redirect to('/')
+  else
+    erb :new_post
+  end
+end
+
+put "/posts/:id/upvote" do
+  post = Post.find(params[:id])
+  post.votes += 1
+  post.save
+  redirect '/'
+end
+
+delete "/posts/:id/delete" do
+  post = Post.find(params[:id])
+  post.destroy
+  post.save
+  redirect "/"
+end
